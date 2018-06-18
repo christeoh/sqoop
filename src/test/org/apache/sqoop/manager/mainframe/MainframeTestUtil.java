34c12869b098a976fac58c287bf579f33e021d8d
Chris Teoh <cteoh@cloudera.com>
2018-06-18 11:43:17 +1000

    Separated plaintext and binary datasets in Test Util

--- src/test/org/apache/sqoop/manager/mainframe/MainframeTestUtil.java
+++ src/test/org/apache/sqoop/manager/mainframe/MainframeTestUtil.java
@@ -50,13 +50,22 @@ public class MainframeTestUtil {
   public static final String EXPECTED_GDG_BINARY_DATASET_MD5 = System.getProperty(
       "sqoop.test.mainframe.ftp.binary.dataset.gdg.md5",
       "43eefbe34e466dd3f65a3e867a60809a");
+  public static final String SEQ_DATASET_NAME = System.getProperty(
+      "sqoop.test.mainframe.ftp.dataset.seq",
+      "TSODIQ1.GDGTEXT.G0001V43");
+  public static final String SEQ_DATASET_FILENAME = System.getProperty(
+      "sqoop.test.mainframe.ftp.dataset.seq.filename",
+      "G0001V43");
+  public static final String EXPECTED_SEQ_DATASET_MD5 = System.getProperty(
+      "sqoop.test.mainframe.ftp.dataset.seq.md5",
+      "f0d0d171fdb8a03dbc1266ed179d7093");
   public static final String SEQ_BINARY_DATASET_NAME = System.getProperty(
       "sqoop.test.mainframe.ftp.binary.dataset.seq",
-      "TSODIQ1.GDGTEXT.G0001V43");
+      "TSODIQ1.FOLDER.FOLDERTXT");
   public static final String SEQ_BINARY_DATASET_FILENAME = System.getProperty(
       "sqoop.test.mainframe.ftp.binary.dataset.seq.filename",
-      "G0001V43");
+      "FOLDERTXT");
   public static final String EXPECTED_SEQ_BINARY_DATASET_MD5 = System.getProperty(
       "sqoop.test.mainframe.ftp.binary.dataset.seq.md5",
-      "f0d0d171fdb8a03dbc1266ed179d7093");
+      "1591c0fcc718fda7e9c1f3561d232b2b");
 }
