f5db2be4d5c9b99a2427531974e70252b2deb12c
Chris Teoh <cteoh@cloudera.com>
2018-08-17 23:14:24 +1000

    Removed unused import

--- src/java/org/apache/sqoop/SqoopOptions.java
+++ src/java/org/apache/sqoop/SqoopOptions.java
@@ -31,7 +31,6 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
